99874a660b032df9e2947363c57d6a051a80f656
Denio Nhanale <dnhanale@gmail.com>
2024-02-23 15:11:27 +0200

    Remove email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,17 +3,17 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { host: 'https://reservation-api-3po5.onrender.com/',  protocol: "https" }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            'dnhanale@gmail.com',
-  password:             'qzcroitvqzmtgphr',
-  authentication:       'plain',
-  enable_starttls_auto: true }
+  # config.action_mailer.default_url_options = { host: 'https://reservation-api-3po5.onrender.com/',  protocol: "https" }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.gmail.com',
+  # port:                 587,
+  # domain:               'gmail.com',
+  # user_name:            'dnhanale@gmail.com',
+  # password:             'qzcroitvqzmtgphr',
+  # authentication:       'plain',
+  # enable_starttls_auto: true }
  
 
   # Code is not reloaded between requests.
@@ -69,11 +69,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "reservationAPI_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
